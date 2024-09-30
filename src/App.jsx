dd50e3e72ae14b16ba334ffd27bbcd97eb6e7ffd
jaheda8256 <jahedasultana47@gmail.com>
2024-09-30 23:21:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductsView from "./features/products/ProductsView";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux Tool Kit</h1>
+      <ProductsView></ProductsView>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/features/products/ProductsView.jsx
@@ -0,0 +1,11 @@
+
+
+const ProductsView = () => {
+    return (
+        <div>
+            <h1>hi</h1>
+        </div>
+    );
+};
+
+export default ProductsView;
