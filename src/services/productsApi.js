6d791df3bfda5b41c5e6d2df0d2c87a2a13d962f
jaheda8256 <jahedasultana47@gmail.com>
2024-10-01 16:50:53 +0600

    added list

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -4,16 +4,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3003/' }),
+
+  tagTypes: ["Products"],
+
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => `products`,
+      providesTags: (result) => result ? [...result.map(({id}) =>({
+        type: "Products", id
+      })),{type: "Products",id: "LIST"}] : [{type: "Products", id: "LIST"}]
     }),
-
+        
     deleteProducts: builder.mutation({
       query: (id) => ({
         url: `products/${id}`,
         method: `DELETE`
       }),
+      invalidatesTags: (result, error, id) =>[{
+        type: "Products",
+        id
+      }]
     }),
   }),
 })
